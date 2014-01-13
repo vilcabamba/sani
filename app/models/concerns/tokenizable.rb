f46e55cf36cac9082ab99de286ec8bf5b597b127
Mario Andrés Correa <admin@macool.me>
2014-01-13 14:29:53 -0500

    added #cached_find_by_token

--- app/models/concerns/tokenizable.rb
+++ app/models/concerns/tokenizable.rb
@@ -10,4 +10,12 @@ module Tokenizable
       self.token = SecureRandom.hex(50)
     end while self.class.exists?(token: token)
   end
+
+  module ClassMethods
+    def cached_find_by_token(token)
+      Rails.cache.fetch [name, "token", token] do
+        find_by token: token
+      end
+    end
+  end
 end
