714144194d71190753d46e0e895ccf02e0193c2e
Mario Andrés Correa <admin@macool.me>
2014-01-13 14:25:38 -0500

    added Tokenizable

---
+++ app/models/concerns/tokenizable.rb
@@ -0,0 +1,13 @@
+module Tokenizable
+  extend ActiveSupport::Concern
+
+  included do
+    before_create :create_token!
+  end
+
+  def create_token!
+    begin
+      self.token = SecureRandom.hex(50)
+    end while self.class.exists?(token: token)
+  end
+end
