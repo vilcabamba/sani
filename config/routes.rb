59f3aa71aca3fb986f9d7da6da1f02472ddd94c5
Mario Andrés Correa <admin@macool.me>
2014-01-14 10:40:09 -0500

    added login routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Sani::Application.routes.draw do
   
   root "home#index"
 
-  get "login" => "sessions#index", as: :login
+  get "login"  => "sessions#index",  as: :login
+  get "logout" => "sessions#logout", as: :logout
   resources :sessions
 
   namespace :api do
