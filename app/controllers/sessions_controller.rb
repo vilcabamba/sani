a5189ed62d60139a6e80c0db08487c8d4d9a6130
Mario Andrés Correa <admin@macool.me>
2014-01-14 10:54:59 -0500

    completed SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
+# encoding: utf-8
 class SessionsController < ApplicationController
 
   def index
+  end
+  def create
+    if user = login(params[:username], params[:password])
+      redirect_back_or_to root_path, notice: "Bienvenido, #{user.username}"
+    else
+      render :index, notice: "Nombre de usuario o contraseña incorrectos"
+    end
+  end
+  def destroy
+    logout
+    redirect_to login_path
   end
 end
