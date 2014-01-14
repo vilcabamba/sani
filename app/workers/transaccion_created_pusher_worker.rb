1479966de943f84032aaf620b56b7e386a4ef93f
Mario Andrés Correa <admin@macool.me>
2014-01-14 13:33:12 -0500

    bugfix

--- app/workers/transaccion_created_pusher_worker.rb
+++ app/workers/transaccion_created_pusher_worker.rb
@@ -5,6 +5,6 @@ class TransaccionCreatedPusherWorker
   def perform(transaccion_id)
     transaccion = Transaccion.cached_find transaccion_id
     business = transaccion.cached_business
-    Pusher[business.token].trigger("new_transaccion")
+    Pusher[business.token].trigger("new_transaccion", {})
   end
 end
