51e87de90f76062fae669a86107167c7b171b0e5
Mario Andrés Correa <admin@macool.me>
2014-01-14 10:53:46 -0500

    bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Sani::Application.routes.draw do
   root "home#index"
 
   get "login"  => "sessions#index",  as: :login
-  get "logout" => "sessions#logout", as: :logout
+  get "logout" => "sessions#destroy", as: :logout
   resources :sessions
 
   namespace :api do
