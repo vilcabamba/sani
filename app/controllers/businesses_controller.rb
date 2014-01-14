347cd72541720607e13d96d1fb31f06d82de5f4b
Mario Andrés Correa <admin@macool.me>
2014-01-14 11:53:18 -0500

    views

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,18 +1,53 @@
 class BusinessesController < ApplicationController
   before_action :require_login, :set_active
+  before_action :find_business, only: [:show, :edit, :update]
 
   def index
     @businesses = Business.all
   end
 
   def show
-    @business = Business.cached_find params[:id]
+  end
+
+  def new
+    @business = Business.new
+    render :edit
+  end
+
+  def edit
+  end
+
+  def create
+    @business = Business.new business_params
+    if @business.save
+      flash.now[:notice] = "Creado"
+      render :show
+    else
+      flash.now[:notice] = "No creado"
+      render :new
+    end
+  end
+
+  def update
+    if @business.update_attributes(business_params)
+      flash.now[:notice] = "Actualizado"
+      render :show
+    else
+      flash.now[:notice] = "No actualizado"
+      render :edit
+    end
   end
 
   private
 
   def set_active
     @navbar_active = "businesses"
+  end
+  def find_business
+    @business = Business.cached_find params[:id]
+  end
+  def business_params
+    params.require(:business).permit(:nombre)
   end
   
 end
