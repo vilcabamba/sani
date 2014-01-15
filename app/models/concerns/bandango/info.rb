d99263ac99048d4dc8dbcd7e7ea73b318d1b58d6
Mario Andrés Correa <admin@macool.me>
2014-01-15 13:22:32 -0500

    added #last_connection and #last_connection_time to Bandango

--- app/models/concerns/bandango/info.rb
+++ app/models/concerns/bandango/info.rb
@@ -8,5 +8,11 @@ class Bandango < ActiveRecord::Base
         name
       end
     end
+    def last_connection_time
+      @last_connection_time ||= last_connection.created_at if last_connection
+    end
+    def last_connection
+      @last_connection ||= connections.last
+    end
   end
 end
