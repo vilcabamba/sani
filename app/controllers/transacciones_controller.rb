4271bc2f6a207f64e9a9b6760e597b055a2b095a
Mario Andrés Correa <admin@macool.me>
2014-01-15 15:16:52 -0500

    TransaccionesController#show

--- app/controllers/transacciones_controller.rb
+++ app/controllers/transacciones_controller.rb
@@ -3,5 +3,10 @@ class TransaccionesController < BusinessController
   def index
     @transacciones = @business.transacciones.includes(:bandango).page(params[:page]).per(10)
   end
+
+  def show
+    @transacciones = Transaccion.where(id: params[:id]).page
+    render :index
+  end
   
 end
