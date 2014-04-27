925acdcbabf9be131c38d3ff559dcf1ecb50b120
Mario Andrés Correa <a@macool.me>
2014-04-26 20:14:08 -0500

    let rails handle errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include Sessions
-  include RenderErrors
   include JSONRequestsForgeryBypass
 
   decent_configuration do
